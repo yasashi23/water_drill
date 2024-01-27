5c833a99a05e9384c6043b3cab07d5145176583d
yasashi <yasashi@pop-os.localdomain>
2024-01-27 20:06:14 +0700

    sedang memanipulasi form data di information-list documment.js 27_jan_2024 20:06

--- public/information-list/script/document.js
+++ public/information-list/script/document.js
@@ -6,21 +6,98 @@ const processScreen = document.querySelector(".edit-process")
 
 const editListData = document.querySelectorAll(".edit-data")
 
-close.addEventListener("click",clickToClose)
+const buttonUpdate = document.querySelector(".btn-update")
 
+const updateForm = document.querySelector("#updateForm")
+
+
+// get data from list data
+const driller = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:first-child")
+const  assistans = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:nth-child(2)")
+const  earn = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:nth-child(3)")
+
+console.log(buttonUpdate)
+
+
+
+
+// single listener
+close.addEventListener("click",()=>clickToClose("off"))
+
+buttonUpdate.addEventListener("click",clickToUpdate)
+
+
+
+// multiple listener
+editListData.forEach(loopEditListData)
 
 
 
 
 // function Listener
 function clickToClose(a){
-    const newClassModal = modalEdit.getAttribute("class")+" off"
+    addOffClass(a)
+}
 
-    const newClassProcess = 
-    processScreen.getAttribute("class")+" off"
+function clickToEdit(i){
 
-    modalEdit.setAttribute("class",newClassModal)
+    addOffClass("")
 
-    processScreen.setAttribute("class",newClassProcess)
+    // console.log(driller[i],assistans[i],earn[i])
+    
+}
+
+function clickToUpdate(a){
+    addOffClass("off")
+    const dataform = serializeFormData(updateForm)
+    console.log(dataform)
+}
+
+
+
+// functions
+function loopEditListData(item,index){
+    item.addEventListener("click",()=>clickToEdit(index))
+}
+
+function addOffClass(a){
+    let newClassModal = modalEdit.getAttribute("class")+` ${a}`
+
+    let newClassProcess = 
+    processScreen.getAttribute("class")+` ${a}`
+
+
+    if(a==="off"){
+        modalEdit.setAttribute("class",newClassModal)
+    
+        processScreen.setAttribute("class",newClassProcess) 
+    }else{
+
+        newClassModal = newClassModal.split(" ")[0]
+        newClassProcess = newClassProcess.split(" ")[0]
+        modalEdit.setAttribute("class",newClassModal)
+        processScreen.setAttribute("class",newClassProcess)
+
+    }
 
 }
+
+function serializeFormData(form) {
+    var formData = new FormData(form);
+    var serializedData = {};
+  
+    for (var [name, value] of formData) {
+      if (serializedData[name]) {
+        if (!Array.isArray(serializedData[name])) {
+          serializedData[name] = [serializedData[name]];
+        }
+        serializedData[name].push(value);
+      } else {
+        serializedData[name] = value;
+      }
+    }
+  
+    return serializedData;
+  }
+
+
