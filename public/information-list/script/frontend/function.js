d7761f4238f4f8852fcdc026a9c37c5f285d39d8
yasashibp <yasashibp@pop-os.localdomain>
2024-01-28 14:37:26 +0700

    login sudah kelar + menggunakan bem, sekarang lg modif completion2 28_jan_2024 14:37

--- public/information-list/script/frontend/function.js
+++ public/information-list/script/frontend/function.js
@@ -1,19 +1,3 @@
-const close = document.querySelector(".close")
-const modalEdit = document.querySelector(".edit-data-modal")
-const processScreen = document.querySelector(".edit-process")
-const editListData = document.querySelectorAll(".edit-data")
-const buttonUpdate = document.querySelector(".btn-update")
-const updateForm = document.querySelector("#updateForm")
-let numberOfDataList
-// get data from list data
-const driller = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:first-child")
-const  assistans = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:nth-child(2)")
-const  earn = document.querySelectorAll(".body-data .data-list tr:nth-child(n+2) td:nth-child(3)")
-// single listener
-close.addEventListener("click",()=>clickToClose("off"))
-buttonUpdate.addEventListener("click",clickToUpdate)
-// multiple listener
-editListData.forEach(loopEditListData)
 // function Listener
 function clickToClose(a){
     addOffClass(a)
@@ -33,6 +17,7 @@ function clickToUpdate(a){
     console.log(driller[i],assistans[i],earn[i])
     console.log(dataform)
   }
+  
 // functions
 function loopEditListData(item,index){
     item.addEventListener("click",()=>clickToEdit(index))
---
+++ public/information-list/script/frontend/listener.js
@@ -0,0 +1,6 @@
+// single listener
+close.addEventListener("click",()=>clickToClose("off"))
+buttonUpdate.addEventListener("click",clickToUpdate)
+
+// multiple listener
+editListData.forEach(loopEditListData)
